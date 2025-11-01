14c77e123ae74eda702b9460aaa774af393d9018
TrungHung07 <nguyenvotrunghung2004@gmail.com>
2025-11-01 22:46:20 +0700

    feat : crud customer

---
+++ StoreManagement/StoreManagement/DTOs/Request/CustomerCreateRequest.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.DTOs.Request
+{
+    public class CustomerCreateRequest
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string? Phone { get; set; }
+
+        [MaxLength(100)]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
