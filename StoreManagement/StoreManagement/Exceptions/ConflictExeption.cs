134a8654d40cba9da2c1816a8fb3268e623e7b71
TrungHung07 <nguyenvotrunghung2004@gmail.com>
2025-11-15 14:57:18 +0700

    chore : refactor response of product service and controller , add exeption

---
+++ StoreManagement/StoreManagement/Exceptions/ConflictExeption.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreManagement.Exceptions
+{
+    public class ConflictExeption : Exception
+    {
+        public ConflictExeption(string message) : base(message)
+        {
+        }
+    }
+}
